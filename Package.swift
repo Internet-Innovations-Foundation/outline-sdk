4c34da2fe511949c3dd8d7c1043e8cf4c857f122
Mikhail Ageev <info@michaelageev.com>
2025-10-03 10:51:01 +0200

    Fix Package config

--- Package.swift
+++ Package.swift
@@ -2,21 +2,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "Outline MobileProxy",
+    name: "OutlineSDK",
     platforms: [
         .iOS(.v15)
     ],
     products: [
         .library(
-            name: "Outline MobileProxy",
-            targets: ["MobileProxyFramework"]
+            name: "OutlineSDK",
+            targets: ["OutlineSDK"]
         ),
     ],
     targets: [
         .binaryTarget(
-            name: "MobileProxyFramework",
+            name: "OutlineSDK",
             url: "https://raw.githubusercontent.com/Internet-Innovations-Foundation/outline-sdk/refs/heads/main/mobileproxy.xcframework.zip",
             checksum: "1c9637f10f4f7534472d159af07b893ff4df5c2f7d1af4ca464868e667ea37a3"
         ),
     ]
-)
+) 
