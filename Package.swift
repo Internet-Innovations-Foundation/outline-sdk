3ccf39cb521a2ab97bf457446dafc8b1778acca9
Mikhail Ageev <info@michaelageev.com>
2025-10-03 11:55:52 +0200

    Package config update

--- Package.swift
+++ Package.swift
@@ -7,16 +7,13 @@ let package = Package(
         .iOS(.v15)
     ],
     products: [
-        .library(
-            name: "Mobileproxy",
-            targets: ["mobileproxy"]
-        ),
+        .library(name: "Mobileproxy", targets: ["mobileproxy"])
     ],
     targets: [
         .binaryTarget(
             name: "mobileproxy",
             url: "https://github.com/Internet-Innovations-Foundation/outline-sdk/releases/download/1.0.6/mobileproxy.xcframework.zip",
             checksum: "1c9637f10f4f7534472d159af07b893ff4df5c2f7d1af4ca464868e667ea37a3"
-        ),
+        )
     ]
 )
