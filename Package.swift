43ac9fd92734439ef0904d0b12d2e30748dd2e59
Mikhail Ageev <info@michaelageev.com>
2025-10-03 11:10:39 +0200

    Swift Package URL fix

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OutlineSDK",
-            url: "https://raw.githubusercontent.com/Internet-Innovations-Foundation/outline-sdk/refs/heads/main/mobileproxy.xcframework.zip",
+            url: "https://github.com/Internet-Innovations-Foundation/outline-sdk/releases/download/1.0.6/mobileproxy.xcframework.zip",
             checksum: "1c9637f10f4f7534472d159af07b893ff4df5c2f7d1af4ca464868e667ea37a3"
         ),
     ]
