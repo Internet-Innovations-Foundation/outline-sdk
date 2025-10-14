7fc81dc2b7830164fed83628be8b70ac775f806e
Mikhail Ageev <info@michaelageev.com>
2025-10-14 18:54:24 +0200

    Update file release URL

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "mobileproxy",
-            url: "https://github.com/Internet-Innovations-Foundation/outline-sdk/releases/download/1.0.7/mobileproxy.xcframework.zip",
+            url: "https://github.com/Internet-Innovations-Foundation/outline-sdk/releases/download/1.0.8/mobileproxy.xcframework.zip",
             checksum: "414b71479d9aebc1adfd341e3083ffd4ffd4e95956a89055577cab0ae29fffc0"
         )
     ]
